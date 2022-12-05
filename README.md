aef9654ebea75cc52630b94b49c46a0e5c8da056
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2022-12-05 01:36:20 +0000

    Flushed out code and updated README.md

--- README.md
+++ README.md
@@ -4,14 +4,30 @@ Create a Neural Network and a program that can make predictions utilizing the Ne
 
 ## Pre-reads
 
-https://learn.ml5js.org/#/reference/neural-network
+* IBM's overview of [Neural Networks](https://www.ibm.com/cloud/learn/neural-networks)
+* ML5 page on their Neural Network feature: https://learn.ml5js.org/#/reference/neural-network
+* Dive into Neural Network Choices (skim)
+  * [Current best answer concerning hidden layers](https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw)
+  * [Configuring layers](https://machinelearningmastery.com/how-to-configure-the-number-of-layers-and-nodes-in-a-neural-network/)
+  * [Activation Functions](https://machinelearningmastery.com/choose-an-activation-function-for-deep-learning/)
+* Peruse the UC-Irvine Machine Learning Repository of datasets: https://archive-beta.ics.uci.edu/datasets. Try filtering by classification problems (though you are free to try regression problems as well). 
 
 ## Starter Code Overview
 
+The code is more or less directly translated from the ml5 page, along with some drawing functionality, and the ability to generate random data with which to use the classifier. 
+
+Here is a description of the dataset. 
+
+> There are 10 predictors, all quantitative, and a binary dependent variable, indicating the presence or absence of breast cancer. The predictors are anthropometric data and parameters which can be gathered in routine blood analysis. Prediction models based on these predictors, if accurate, can potentially be used as a biomarker of breast cancer. Quantitative Attributes: Age (years) BMI (kg/m2) Glucose (mg/dL) Insulin (µU/mL) HOMA Leptin (ng/mL) Adiponectin (µg/mL) Resistin (ng/mL) MCP-1(pg/dL) Labels: 1=Healthy controls 2=Patients. [Example data source]( https://archive-beta.ics.uci.edu/dataset/451/breast+cancer+coimbra)
+
 ## Assignment 
+
+Find a dataset, build a neural network with it, and make classifications with it. 
 
 ### Requirements
 
 ### _Some_ Ideas for Ways to Expand on This Project. 
+
+A more full-featured input method (text input, sliders)
 
 <!--- Footnotes Below --->
--- script.js
+++ script.js
@@ -1,12 +1,127 @@
-const colorList = ['maroon','brown','olive','teal','navy','black','red','orange','yellow','lime','green','cyan','blue','purple','magenta','grey','pink','apricot','beige','mint','lavender','white'];
+const colorList = ['maroon','brown','olive','teal','navy','red','orange','yellow','lime'];
+
+let data;
+let stats;
+let classification;
+
+// Step 2: set your neural network options (Step 1 is in preload)
+const neuralNetworkOptions = {
+  task: 'classification',
+  debug: true
+}
+
+const nueralNetworkTrainingOptions = {
+  epochs: 64,
+  batchSize: 12
+}
+
+// Step 3: initialize your neural network
+const nn = ml5.neuralNetwork(neuralNetworkOptions);
+
+function preload() {
+  // Step 1: load data or create some data 
+  loadTable('data.csv', 'csv', 'header', (table) => { // this runs after the table gets loaded. 
+    data = table;
+    stats = table
+    .columns
+    .filter( c => c != 'Classification' )
+    .reduce( (obj,c) => {
+      obj[c] = {min:Infinity, max:-Infinity}
+      return obj;
+    }, {} )
+    // Step 4: add data to the neural network
+    for( const row of table.getRows() ) {
+      const input = {}
+      table
+      .columns
+      .filter( c => c != 'Classification' )
+      .forEach( c => {
+        const value = row.getNum(c); 
+        stats[c].min = min(stats[c].min, value);
+        stats[c].max = max(stats[c].max, value);
+        input[c] = value
+      })
+      const output = { classification: row.get('Classification') }
+      nn.addData(input,output);
+    }    
+  });
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  stroke(random(colorList));
+  // Step 5: normalize your data;
+  nn.normalizeData();
+  // Step 6: train your neural network
+  // Step 7: use the trained model by calling
+  //   makeRandomClassification upon training completion.
+  nn.train(nueralNetworkTrainingOptions, makeRandomClassification);
+  noStroke();
   fill(random(colorList));  
   background(255);
+  noLoop();
 }
 
 function draw() {
-  ellipse(mouseX, mouseY, 25, 25);
+  background('white');
+  if( classification ) {
+    fill('black')
+    text(`Result: ${classification.result} (${classification.confidence}%)`, 50, 20 );
+    const barHeight = (height - 100 - (data.columns.length-2)*10) / (data.columns.length-1);
+    const barWidth = width - 100;
+    const x = 50;
+    let y = 50;
+    for( let i = 0; i < data.columns.length-1; i++ ) {      
+      const c = data.columns[i]
+      fill(colorList[i]);
+      rect(x,y,barWidth,barHeight);            
+      markerX = map( classification.input[c],
+        stats[c].min, stats[c].max,
+        50,width-50 );
+      noStroke();
+      fill('black');
+      rect( markerX - 5, y - 2, 10, barHeight + 4 );
+      push();
+      translate(x-2,y+barHeight);
+      rotate(-PI/2);
+      text(c,0,0)
+      pop();
+      y += barHeight + 10;
+    }
+  }
+}
+
+function keyPressed() {
+  if( key === 'r' ) {
+    makeRandomClassification();
+  }
+}
+
+// Step 8: make a classification
+function makeRandomClassification() {
+  classification = {};
+  let input = generateRandomInput();
+  classification.input = input; 
+  nn.classify(input, handleResults);    
+}
+
+// Step 9: define a function to handle the results of your classification
+function handleResults(error, result) {
+  if(error){
+    console.error(error);
+  } else {
+    console.log(result)
+    classification.result = result[0].label === "2"
+    classification.confidence = round(result[0].confidence * 100, 2);
+    redraw();
+  }
+}
+
+function generateRandomInput() {
+  return data
+  .columns
+  .filter( c => c != 'Classification' )
+  .reduce( (obj,c) => {
+    obj[c] = random(stats[c].min,stats[c].max)
+    return obj;
+  }, {} )  
 }
